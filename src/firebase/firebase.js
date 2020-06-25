d467cafa5cc11527c73eb5e477be6d2667a3d2b2
Joshua <fujin95@gmail.com>
2020-06-24 23:56:48 -0400

    hardcoded firebase database url into config fil

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -4,7 +4,7 @@ import 'firebase/auth';
 const firebaseConfig = {
 	apiKey: process.env.FIREBASE_API_KEY,
 	authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-	databaseURL: process.env.FIREBASE_DATABASE_URL,
+	databaseURL: 'https://expensify-bcb9e.firebaseio.com',
 	projectId: process.env.FIREBASE_PROJECT_ID,
 	storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
 	messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
